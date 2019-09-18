713e09868281e3e255769b9466d42adafcd9c765
Maxim Alexeev <possumboy333@gmail.com>
2019-09-19 04:49:38 +0500

    init commin

---
+++ my_max.py
@@ -0,0 +1,9 @@
+x= int(input("Введите первое число: "))
+y= int(input("Введите второе число: "))
+def nmbrs(x, y):
+    if x>y:
+        return ("Максимальное число:",x)
+    else:
+        return ("Максимальное число:",y)
+print (nmbrs)
+
---
+++ table.py
@@ -0,0 +1,14 @@
+num= int(input( "Введите номер:"))
+if num == 3:
+    print ("Li(Литий)")
+if num == 17:
+    print("Cl(Хлор)")
+if num == 25:
+    print("Mn(Марганец)")
+if num == 80:
+    print ("Hg(Ртуть)")
+else:
+    print ("Данных об элементе нет")
+
+
+
