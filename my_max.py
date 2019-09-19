0765729e89c29fc0d1c10f02ffdada451ccadf0e
maxim492 <55504970+maxim492@users.noreply.github.com>
2019-09-19 05:21:35 +0500

    Update my_max.py

--- my_max.py
+++ my_max.py
@@ -1,9 +1,6 @@
-x= int(input("Введите первое число: "))
-y= int(input("Введите второе число: "))
 def nmbrs(x, y):
     if x>y:
         return ("Максимальное число:",x)
     else:
         return ("Максимальное число:",y)
-print (nmbrs)
 
